8903eefce8af3d2a104595947a2d8fdb8c67fc87
Javi Ansoleaga <javi_am12@hotmail.com>
2015-03-10 18:27:19 +0100

    Unity Arts

---
+++ RescueLander Unity/Assets/Scripts/Play_anim.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Play_anim : MonoBehaviour
+{
+	public float amplitude = 1.0f;
+	public float omega = 1.0f;
+	float index;
+	
+	public void Update()
+	{
+		index += Time.deltaTime;
+		float x = 0;
+
+		if(x > 0)
+		{
+			x = amplitude * Mathf.Cos (omega * index);
+		}
+		transform.localScale = new Vector3(Mathf.Clamp(x, 0.5f, 1.5f),Mathf.Clamp(x, 0.5f, 1.5f),0);
+	}
+}
---
+++ RescueLander Unity/Assets/Scripts/Start_Anim.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Start_Anim : MonoBehaviour
+{
+
+	public float amplitudeX = 10.0f;
+	public float amplitudeY = 5.0f;
+	public float omegaX = 1.0f;
+	public float omegaY = 5.0f;
+	float index;
+	
+	public void Update()
+	{
+		index += Time.deltaTime;
+		float x = amplitudeX*Mathf.Cos (omegaX*index);
+		float y = amplitudeY*Mathf.Sin (omegaY*index);
+		transform.localPosition = new Vector3(x,y,0);
+	}
+}
