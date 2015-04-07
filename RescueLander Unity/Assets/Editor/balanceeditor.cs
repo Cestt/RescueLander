884cc8e76133f3df4329e41d5c4b31b00018f1c0
Cestt <electromanuel33@hotmail.com>
2015-04-07 19:51:04 +0200

    Update

---
+++ RescueLander Unity/Assets/Editor/balanceeditor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Movement))]
+public class balanceeditor : Editor
+{
+	public Movement BalanceScript;
+	
+	public override void OnInspectorGUI()
+	{
+
+
+		DrawDefaultInspector();
+		
+
+		if(GUILayout.Button("Javi pulsa aqui"))
+		{
+
+			BalanceScript.BalanceCalc();
+			EditorUtility.SetDirty(BalanceScript);
+
+		}
+	}
+	public void Awake(){
+		BalanceScript = (Movement) target;
+	}
+}
---
+++ RescueLander Unity/Assets/Scripts/Camera/Cameraposition.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cameraposition : MonoBehaviour {
+
+	public GameObject ship;
+	public int maxScrollX;
+	private Camera cam;
+	private Transform shipPos;
+	private Vector3 tempPos;
+	private tk2dCamera camera;
+	 
+	public bool BoundedScroll = true;
+	// Use this for initialization
+	void Awake () {
+
+		camera = this.GetComponent<tk2dCamera>();
+		cam = Camera.main;
+		shipPos = ship.transform;
+
+	}
+
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		Follow();
+
+	
+	}
+
+	void Follow(){
+
+
+		if(ship != null){
+
+			if(BoundedScroll){
+				tempPos = cam.transform.position;
+				
+			
+					tempPos.x = shipPos.position.x;
+					tempPos.y = shipPos.position.y;
+					cam.transform.position = Vector3.MoveTowards(transform.position,tempPos,500*Time.deltaTime);
+
+
+				cam.transform.position = 
+					new Vector3(Mathf.Clamp(cam.transform.position.x,camera.nativeResolutionWidth/2/camera.ZoomFactor,
+					                        maxScrollX - camera.nativeResolutionWidth/2/camera.ZoomFactor),
+					            Mathf.Clamp(cam.transform.position.y,0 + camera.nativeResolutionHeight/2/camera.ZoomFactor,1080 - camera.nativeResolutionHeight/2/camera.ZoomFactor),
+					            cam.transform.position.z);
+			}else{
+				
+				tempPos = cam.transform.position;
+				tempPos.x = shipPos.position.x - cam.pixelWidth;
+				tempPos.y = shipPos.position.y - cam.pixelHeight;
+				
+				cam.transform.position = tempPos;
+			}
+
+		}
+
+
+	}
+
+
+}
