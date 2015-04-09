69483e0223229d590a66b58c7952f278e701a599
Cestt <electromanuel33@hotmail.com>
2015-04-09 17:56:09 +0200

    Redo

---
+++ RescueLander Unity/Assets/Scripts/AstroIcos.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AstroIcos : MonoBehaviour
+{
+	public GameObject astroIco1;
+	public GameObject astroIco2;
+	public GameObject astroIco3;
+
+	ShipAstronautPickUp shipAstronautPickUp;
+	// Use this for initialization
+	void Awake ()
+	{
+		shipAstronautPickUp = GameObject.Find ("Ship").GetComponent<ShipAstronautPickUp> ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		switch (shipAstronautPickUp.astronautPicked)
+		{
+			case 1:
+				astroIco1.GetComponent<tk2dSprite>().SetSprite("Astronaut_Ico");
+				break;
+			case 2:
+				astroIco2.GetComponent<tk2dSprite>().SetSprite("Astronaut_Ico");
+				break;
+			case 3:
+				astroIco3.GetComponent<tk2dSprite>().SetSprite("Astronaut_Ico");
+				break;
+		}
+	}
+}
---
+++ RescueLander Unity/Assets/Scripts/Nave/ShipSmoke.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipSmoke : MonoBehaviour {
+
+	public GameObject ship;
+	private Movement movement;
+	private ParticleSystem particlesystem;
+	// Use this for initialization
+	void Awake () {
+	
+		movement = ship.GetComponent<Movement>();
+		particlesystem = GetComponent<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if(movement.motor){
+			particlesystem.enableEmission = true;
+		}
+		if(!movement.motor){
+			particlesystem.enableEmission = false;
+		}
+
+	}
+
+}
