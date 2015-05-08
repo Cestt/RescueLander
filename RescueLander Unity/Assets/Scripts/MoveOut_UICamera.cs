3815ecc751e9587c2affd0de887f71d196dba010
Javi Ansoleaga <javi_am12@hotmail.com>
2015-05-08 16:47:50 +0200

    Unity Arts v.02.05.028

---
+++ RescueLander Unity/Assets/Scripts/MoveOut_UICamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveOut_UICamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.transform.position = new Vector3(gameObject.transform.position.x, -1500);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
