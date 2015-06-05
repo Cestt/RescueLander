57e7a9047ac158b54027cd601f55096457b0918e
sheikkan <aaronsanse@gmail.com>
2015-06-05 20:34:24 +0200

    Script Texto Score Time

---
+++ RescueLander Unity/Assets/Scripts/Score/ScoreTextTime.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreTextTime : MonoBehaviour {
+
+	tk2dTextMesh textMesh;
+	ScoreManager scoreManager;
+
+	void Awake(){
+		textMesh = GetComponent<tk2dTextMesh>();
+		scoreManager = GameObject.Find("ScoreCoin_Manager").GetComponent<ScoreManager> ();
+	}
+
+	void Update(){
+		textMesh.text = "Score: "+scoreManager.timeScore.ToString();
+	}
+}
