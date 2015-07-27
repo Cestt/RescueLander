6e54c600cbc6806049e393dc8c6b7a791ecbc1bd
sheikkan <aaronsanse@gmail.com>
2015-07-27 03:18:57 +0200

    Aaron Code 07 Fantasma de nivel 1 Ice

---
+++ RescueLander Unity/Assets/Scripts/Tutorial/IceGhost.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IceGhost : MonoBehaviour {
+
+	GameObject ship;
+	GameObject ghost;
+	float time;
+
+	void Awake(){
+		ghost = GameObject.Find ("UI_Camera").transform.FindChild ("Tuto_IceBlock").gameObject;
+		ship = GameObject.Find (dataManger.manager.actualShip + "(Clone)");
+		time = 0;
+	}
+
+	void OnTriggerStay2D(Collider2D coll) {
+		if (coll.gameObject.name == "Feet" & (ship.gameObject.transform.eulerAngles.magnitude < 50 || ship.gameObject.transform.eulerAngles.magnitude > 310)){
+			time+= Time.deltaTime;
+			if (time > 1){
+				ghost.SetActive(false);
+				Destroy(this);
+			}
+		}
+	}
+}
